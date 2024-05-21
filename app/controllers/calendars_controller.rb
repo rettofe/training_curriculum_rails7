b53464cd2c31de0c69ac52ae613ae06fbf6b1898
rettofe <ferettosan@gmail.com>
2024-05-21 15:12:30 +0900

    修正4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,6 @@
 class CalendarsController < ApplicationController
   def index
-    getWeek
+    get_Week
     @plan = Plan.new
   end
 
@@ -32,7 +32,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
